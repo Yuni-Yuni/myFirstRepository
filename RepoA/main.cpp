c5ad024ec8bbfea74f491e8ab1e8e99d87c84a4c
YunYun <sakuralove72@mail.ru>
2022-05-25 23:11:13 +0300

    b

---
+++ RepoA/main.cpp
@@ -0,0 +1,15 @@
+#include <C:\Users\Юлия\Desktop\lab-8\RepoA\sdt.h>
+#include <stdio.h>
+main()
+{
+    //UCHEBNAYA PROGRAMMA
+    int a,b;
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+    printf("\nEnter the second number: ");
+    scanf("%d", &b);
+    printf("\nSumma = %d .", a+b);
+    printf("\nRaznost = %d .", a-b);
+    return 0;
+
+}
---
+++ RepoB/main.cpp
@@ -0,0 +1,14 @@
+#include <C:\Users\Юлия\Desktop\lab-8\RepoB\sdt.h>
+#include <stdio.h>
+main()
+{
+    int a,b;
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+    printf("\nEnter the second number: ");
+    scanf("%d", &b);
+    printf("\nSumma = %d .", a+b);
+    printf("\nRaznost = %d .", a-b);
+    return 0;
+
+}
